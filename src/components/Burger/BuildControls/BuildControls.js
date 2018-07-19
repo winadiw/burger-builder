5317f071b83b1684dd056a8855e8e0e9965abd97
winadilaku6 <winadi@laku6.com>
2018-07-19 14:05:54 +0700

    video #117 - Removing Ingredients Safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -6,13 +6,18 @@ const controls = [
     { label: 'Salad', type: 'salad'},
     { label: 'Bacon', type: 'bacon'},
     { label: 'Cheese', type: 'cheese'},
-    { label: 'Meat', type: 'Meat'}
+    { label: 'Meat', type: 'meat'}
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl 
+            key={ctrl.label} 
+            label={ctrl.label}
+            added={() => props.ingredientAdded(ctrl.type)}
+            removed={() => props.ingredientRemoved(ctrl.type)} 
+            disabled={props.disabled[ctrl.type]}/>
         ))}
     </div>
 );
