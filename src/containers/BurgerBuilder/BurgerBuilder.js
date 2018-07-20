79dcc593844f6bf28a1d3dcbca56badaa83c2aa2
winadilaku6 <winadi@laku6.com>
2018-07-20 12:01:11 +0700

    video #165 - Handling Errors withErrorHandler HOC

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -114,7 +115,7 @@ class BurgerBuilder extends Component {
         email: "test@test.com"
       },
       deliveryMethod: "fastest"
-    };
+    };  
     axios
       .post("/orders.json", order)
       .then(response => {
@@ -174,4 +175,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
