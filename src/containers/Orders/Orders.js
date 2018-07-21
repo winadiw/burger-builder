fe4a5b99c3a11aa304adb73379bac10afabc3452
Winadi Wiratama <winadiw@gmail.com>
2018-07-21 16:21:27 +0700

    video #210 - Implementing Navigation Links

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
 
-class Order extends Component {
+class Orders extends Component {
   state = {};
   render() {
     return (
@@ -13,4 +13,4 @@ class Order extends Component {
   }
 }
 
-export default Order;
+export default Orders;
