3e6dae7337185f85ac3406d57b99b1f5af605f1e
Winadi Wiratama <winadiw@gmail.com>
2018-07-21 16:14:24 +0700

    video #209 - Adding an Orders Page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Order from "../../components/Order/Order";
+
+class Order extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
+  }
+}
+
+export default Order;
