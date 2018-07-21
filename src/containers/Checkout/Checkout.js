c34add832c514073f28901fc1809d192170582c3
Winadi Wiratama <winadiw@gmail.com>
2018-07-21 15:45:48 +0700

    video #207 - Navigating to the Contact Data Component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,47 +1,53 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
-
-    state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1
     }
+  };
 
-    componentDidMount() {
-      console.log(this.state.ingredients);
-      const query = new URLSearchParams(this.props.location.search);
-      const ingredients = {};
+  componentDidMount() {
+    console.log(this.state.ingredients);
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
 
-      for(let param of query.entries()) {
-        // ['salad', '1']
-        ingredients[param[0]] = +param[1];
-      }
-      console.log(ingredients);
-      this.setState({
-        ingredients: ingredients
-      })
-    }    
-
-    checkoutCancelledHandler = () => {
-        this.props.history.goBack();
+    for (let param of query.entries()) {
+      // ['salad', '1']
+      ingredients[param[0]] = +param[1];
     }
+    console.log(ingredients);
+    this.setState({
+      ingredients: ingredients
+    });
+  }
 
-    checkoutContinuedHandler = () => {
-        this.props.history.replace('/checkout/contact-data');
-    }
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
 
   render() {
     return (
       <div>
-        <CheckoutSummary 
-        ingredients={this.state.ingredients}
-        checkoutCancelled={this.checkoutCancelledHandler}
-        checkoutContinued={this.checkoutContinuedHandler}/>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
+        />
       </div>
     );
   }
