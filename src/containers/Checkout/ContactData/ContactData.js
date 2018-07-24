d8a570e74f305b28cccf3ad2eb285ae85b68d441
winadilaku6 <winadi@laku6.com>
2018-07-24 11:07:01 +0700

    video #230 - Wrap Up

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -132,6 +132,10 @@ class ContactData extends Component {
   checkValidity = (value, rules) => {
     let isValid = true;
     
+    if(!rules) {
+      return true;
+    }
+    
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;
     }
