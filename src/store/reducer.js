7a4bb595048b6abbfb35eff42a70f1a01ed77ecf
winadilaku6 <winadi@laku6.com>
2018-07-30 17:36:36 +0700

    video #254 - Installing Redux and React Redux

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case ''
+    }
+}
+
+export default reducer;
